69fe5212d30a0661e9c0c23ca7c85a9a0f41404d
abdelmanem <abdelmanem@msn.com>
2025-09-12 22:58:45 +0300

    commit

--- healthclub/settings.py
+++ healthclub/settings.py
@@ -93,8 +93,8 @@ if os.environ.get("DJANGO_DB", "sqlite") == "postgres":
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('POSTGRES_DB', 'healthclub'),
-            'USER': os.environ.get('POSTGRES_USER', 'postgres'),
-            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
+            'USER': os.environ.get('POSTGRES_USER', 'hcuserdb'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'Radisson123'),
             'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
             'PORT': os.environ.get('POSTGRES_PORT', '5432'),
         }
